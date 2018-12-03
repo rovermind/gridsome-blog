f6d64cb557f64c1efb4e4b62e73136be4b03fcb6
안병은 <zentlekimbba@gmail.com>
2018-12-03 18:41:09 +0900

    add dependencies, edit gridsome config

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,3 +1,20 @@
 module.exports = {
-  plugins: []
+  // siteName: "",
+  // siteUrl: "",
+  // siteDescription: "Run blog by Gridsome and Vue.js",
+  // icon: "",
+  transformers: {
+    remark: {}
+  },
+
+  plugins: [
+    {
+      use: "@gridsome/source-filesystem",
+      options: {
+        path: "blog/**/*.md",
+        typeName: "Post",
+        route: "/blog/:slug"
+      }
+    }
+  ]
 }
